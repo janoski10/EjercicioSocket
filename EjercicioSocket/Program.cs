7e23142462f98a6cdf606d341c2186f204ae0f22
janoski10 <alejandro.acuna09@inacapmail.cl>
2022-04-25 17:59:46 -0400

    Agregar archivos de proyecto.

---
+++ EjercicioSocket/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServidorSocketUtil
+{
+    class Program
+    {
+        private int puerto;
+        private Socket servidor;
+
+        static void GenerarComunicacion(ClienteCom clienteCom)
+        {
+            bool terminar = false;
+            while (!terminar)
+            {
+                string mensaje = clienteCom.Leer();
+                if (mensaje != null)
+                {
+                    Console.WriteLine("C: {0}", mensaje);
+                    if (mensaje.ToLower() == "chao")
+                    {
+                        terminar = true;
+                    }
+                    else
+                    {
+                        Console.Write("Ingrese Respuesta: ");
+                        mensaje = Console.ReadLine().Trim();
+                        clienteCom.Escribir(mensaje);
+                        if (mensaje.ToLower() == "chao")
+                        {
+                            terminar = true;
+                        }
+                    }
+
+                }else
+                {
+                    terminar = false;
+                }
+                if (terminar)
+                {
+                    clienteCom.Desconectar();
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            int puerto = Convert.ToInt32(ConfigurationManager.AppSettings["puerto"]);
+            Console.WriteLine("Iniciando Servidor en puerto {0}", puerto);
+            ServerSocket servidor = new ServerSocket(puerto);
+
+            if (servidor.Iniciar())
+            {
+                Console.WriteLine("Servidor Iniciado");
+                while (true)
+                {
+                    Console.WriteLine("Esperando Cliente");
+                    Socket socketCliente = servidor.ObtenerCliente();
+
+                    ClienteCom clienteCom = new ClienteCom(socketCliente);
+                    GenerarComunicacion(clienteCom);
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error, el puerto {0} esta en uso", puerto);
+            }
+        }
+    }
+}
---
+++ ServidorSocketUtil/ServerSocket.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServidorSocketUtil
+{
+    public class ServerSocket
+    {
+        private int puerto;
+        private Socket servidor;
+
+
+        public ServerSocket(int puerto)
+        {
+            this.puerto = puerto;
+        }
+
+        
+        public bool Iniciar()
+        {
+            try
+            {
+                this.servidor = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                
+                this.servidor.Bind(new IPEndPoint(IPAddress.Any, this.puerto));
+                
+                this.servidor.Listen(10);
+
+                return true;
+            }
+            catch (SocketException ex)
+            {
+                return false;
+            }
+        }
+
+        public Socket ObtenerCliente()
+        {
+            return this.servidor.Accept();
+        }
+        public void Cerrar()
+        {
+            try
+            {
+                this.servidor.Close();
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+        }
+    }
+}
